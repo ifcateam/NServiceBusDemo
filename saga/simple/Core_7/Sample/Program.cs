1495fd06207338eb84ba58cf0c58e5d7aa8a2f9f
Zhe Zhang <191679972@qq.com>
2019-03-19 17:43:28 +0800

    rabbitmq  is ok

--- saga/simple/Core_7/Sample/Program.cs
+++ saga/simple/Core_7/Sample/Program.cs
@@ -1,20 +1,53 @@
 ﻿using System;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 using NServiceBus;
+using NServiceBus.Persistence.Sql;
 
 class Program
 {
     static async Task Main()
     {
-        Console.Title = "Samples.SimpleSaga";
-        var endpointConfiguration = new EndpointConfiguration("Samples.SimpleSaga");
+        Console.Title = "Samples.SimpleSaga rabbitmq&mysql";
+        var endpointConfiguration = new EndpointConfiguration("quarrierSamples.SimpleSaga");
 
-        #region config
+        #region Persistence
 
-        endpointConfiguration.UsePersistence<LearningPersistence>();
-        endpointConfiguration.UseTransport<LearningTransport>();
+        
+
+        var sqlPersistence =
+            endpointConfiguration.UsePersistence<SqlPersistence>();
+
+        var connection = $"server=192.168.137.51;user=root;database=new_schema_Test;port=3306;password=123456;AllowUserVariables=True;AutoEnlist=false";
+        sqlPersistence.SqlDialect<SqlDialect.MySql>();
+        sqlPersistence.ConnectionBuilder(
+            connectionBuilder: () =>
+            {
+                return new MySqlConnection(connection);
+            });
+        
+        
+        var subscriptions = sqlPersistence.SubscriptionSettings();
+        subscriptions.CacheFor(TimeSpan.FromMinutes(1));
+
+        endpointConfiguration
+            .UsePersistence<InMemoryPersistence, StorageType.Subscriptions>();
+        
 
         #endregion
+
+
+        #region Transport
+        var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+
+        transport.ConnectionString("host=192.168.137.51;" +
+                                   "username=admin;" +
+                                   "password=admin");
+        transport.UseConventionalRoutingTopology();
+
+        #endregion
+
+        endpointConfiguration.EnableInstallers();
 
         var endpointInstance = await Endpoint.Start(endpointConfiguration)
             .ConfigureAwait(false);
